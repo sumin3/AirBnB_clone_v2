7bb1770c6b608ba23db2c123725940f3942e6102
sumin3 <suminyu@yahoo.com>
2019-01-15 00:15:58 +0000

    init commit

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Write a Bash script that sets up your web servers for the deployment of web_static
+if [ ! -x /usr/sbin/nginx ]
+then
+    apt-get -y update
+    apt-get -y install nginx
+fi
+
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+touch /data/web_static/releases/test/index.html
+echo "Holberton School!" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown ubuntu:ubuntu /data/
+file="/etc/nginx/sites-available/default"
+if ! grep -q "location /hbnb_static {" $file
+then
+    sed -i '/^\tserver_name localhost;/ a\\n\tlocation \/hbnb_static \{\n\t\talias /data/web_static/current/;\n\t\}\n' $file
+    service nginx restart
+fi
